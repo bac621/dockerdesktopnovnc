96c6043af2e46b0af1e7235ecc4ac6394d5239ea
atomney <atomney@gmail.com>
2016-06-05 18:46:27 -0700

    Added VNC access to container

--- StartDockerDesktopNOVNC.sh
+++ StartDockerDesktopNOVNC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -td -v /etc/localtime:/etc/localtime:ro -p 80:6080 atomney/dockerdesktopnovnc
+docker run -td -v /etc/localtime:/etc/localtime:ro -p 80:6080 -p 5900:5900 atomney/dockerdesktopnovnc

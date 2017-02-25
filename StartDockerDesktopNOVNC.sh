a23717c2ef13c8fb17cb0588a822b482cd481c78
atomney <atomney@gmail.com>
2017-02-25 12:33:17 -0800

    Update StartDockerDesktopNOVNC.sh

--- StartDockerDesktopNOVNC.sh
+++ StartDockerDesktopNOVNC.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
-docker run -td -v /etc/localtime:/etc/localtime:ro -p 80:6080 -p 5900:5900 atomney/dockerdesktopnovnc
+
+########## VARIABLES
+
+# NAMES
+D1NAME=D1
+D2NAME=D2
+
+# PORTS
+D1PORT=172.17.0.1:5901
+D2PORT=172.17.0.1:5902
+
+# MEMORY FOR EACH DESKTOP
+MEMORY=512m
+
+if [ "$1" = "--d1" ]; then
+
+        docker run -td -v /etc/localtime:/etc/localtime:ro -p $D1PORT:5900 --name $D1NAME -m $MEMORY atomney/dockerd$
+fi
+
+if [ "$1" = "--d2" ]; then
+
+        docker run -td -v /etc/localtime:/etc/localtime:ro -p $D2PORT:5900 --name $D2NAME -m $MEMORY atomney/dockerd$
+fi
+
+if [ "$1" = "--all" ]; then
+
+        bash $0 --d1
+        bash $0 --d2
+fi
+
+if [ "$1" = "--remove" ]; then
+
+        docker stop $D1NAME
+        docker stop $D2NAME
+        docker rm $D1NAME
+        docker rm $D2NAME
+fi
+
+if [ "$1" = "--pull" ]; then
+
+        docker pull atomney/dockerdesktopnovnc
+        bash $0 --remove
+        bash $0 --all
+fi

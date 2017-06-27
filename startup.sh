0e29139246a1eb8d74b4172e1fd798f29b14cb0a
atomney <atomney@gmail.com>
2017-06-27 16:21:12 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-## Explanation of steps
-# Tell it what display to use
-# Create the new display using xvfb
-# Wait 5 seconds
-# Start LXDE
-# Start VNC Server
-
-export DISPLAY=:1
-Xvfb :1 -screen 0 1600x900x16 &
-sleep 5
-startlxde&
-x11vnc -display :1 -nopw -xkb -ncache_cr -forever -shared &
+echo $VNCPASS | vncpasswd -f > /$HOME/.vnc/passwd &
+chmod 600 $HOME/.vnc/passwd &
+vncserver :0 -SecurityTypes VncAuth -AcceptSetDesktopSize=1 &
 cd /root/noVNC && ln -s vnc.html index.html && ./utils/launch.sh --web /root/noVNC --vnc localhost:5900

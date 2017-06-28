eb3781243e7ba549c3bb14747a28dd938ec1fa08
atomney <atomney@gmail.com>
2017-06-28 10:26:30 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $VNCPASS | vncpasswd -f > /$HOME/.vnc/passwd &
-chmod 600 $HOME/.vnc/passwd &
-vncserver :0 -SecurityTypes VncAuth -AcceptSetDesktopSize=1 &
+su - tux -c "echo $VNCPASS | vncpasswd -f > /home/tux/.vnc/passwd" &&
+su - tux -c "chmod 600 /home/tux/.vnc/passwd" &&
+su - tux -c "vncserver :0 -SecurityTypes VncAuth -AcceptSetDesktopSize=1" &&
 cd /root/noVNC && ln -s vnc.html index.html && ./utils/launch.sh --web /root/noVNC --vnc localhost:5900

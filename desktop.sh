ec6b9e07770415c3b4be40b2e2e2712aee120c7f
atomney <atomney@gmail.com>
2017-07-03 11:40:42 -0700

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -8,13 +8,16 @@ HERE=`pwd`
 D1NAME=d1
 
 # PORTS
-D1PORT=8080
+D1PORT=8079:6080
+D1SSH=172.17.0.1:8080:22
+D1WEB=8081:8081
+D1SOUND=8082:8082
 
 # VNC PASSWORD
 VNCPASS=YOURPASS
 
 # MEMORY FOR EACH DESKTOP
-MEMORY=512m
+MEMORY=128m
 
 # IMAGE NAME
 IMAGENAME=atomney/dockerdesktopnovnc
@@ -25,26 +28,61 @@ mkdir -p $HERE/data
 # START THE CONTAINER
 if [ "$1" = "--start" ]; then
 
- docker run --rm -v /etc/localtime:/etc/localtime:ro -p $D1PORT:6080 \
+ docker run --rm -v /etc/localtime:/etc/localtime:ro -p $D1PORT \
+        -v $HERE/startup.sh:/startup.sh \
+        -p $D1SSH -p $D1WEB -p $D1SOUND \
+        -v /dev/shm:/dev/shm \
+        --device /dev/snd \
+        --device /dev/dri \
+        --group-add audio \
+        --cap-add=NET_ADMIN \
+        -v $HERE/self.pem:/root/noVNC/self.pem \
         -v $HERE/data:/home/tux/data \
-        -e VNCPASS=$VNCPASS --name $D1NAME -m $MEMORY $IMAGENAME &
+        -e VNCPASS=$VNCPASS --name $D1NAME -h $D1NAME -m $MEMORY $IMAGENAME &
 fi
 
 # START THE CONTAINER WITH CERT
-if [ "$1" = "--startwithcert" ]; then
+if [ "$1" = "--startnossl" ]; then
 
- docker run --rm -v /etc/localtime:/etc/localtime:ro -p $D1PORT:6080 \
-        -v $HERE/self.pem:/root/noVNC/self.pem \
+ docker run --rm -v /etc/localtime:/etc/localtime:ro -p $D1PORT \
+        -p $D1SSH -p $D1WEB -p $D1SOUND \
+        -v /dev/shm:/dev/shm \
+        --device /dev/snd \
+        --device /dev/dri \
+        --group-add audio \
+        --cap-add=NET_ADMIN \
         -v $HERE/data:/home/tux/data \
-        -e VNCPASS=$VNCPASS --name $D1NAME -m $MEMORY $IMAGENAME &
-fi
+        -e VNCPASS=$VNCPASS --name $D1NAME -h $D1NAME -m $MEMORY $IMAGENAME &
 
+fi
+# START THE CONTAINER WITHOUT SSL
+if [ "$1" = "--startnossl" ]; then
+
+ docker run --rm -v /etc/localtime:/etc/localtime:ro -p $D1PORT \
+        -p $D1SSH -p $D1WEB -p $D1SOUND \
+        -v /dev/shm:/dev/shm \
+        --device /dev/snd \
+        --device /dev/dri \
+        --group-add audio \
+        --cap-add=NET_ADMIN \
+        -v $HERE/data:/home/tux/data \
+        -e VNCPASS=$VNCPASS --name $D1NAME -h $D1NAME -m $MEMORY $IMAGENAME &
+
+fi
 
 # STOP AND REMOVE THE CONTAINER
 if [ "$1" = "--remove" ]; then
 
         docker stop $D1NAME
         docker rm $D1NAME
+fi
+
+# RESTART THE CONTAINER
+if [ "$1" = "--restart" ]; then
+
+        bash $0 --remove &&
+        sleep 2
+        bash $0 --start
 fi
 
 # PULL THE NEWEST CONTAINER IMAGE
